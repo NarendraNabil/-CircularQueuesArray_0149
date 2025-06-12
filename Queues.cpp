d287a05ec54bc1fc4381334e6acf53fb439363f9
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-12 16:52:10 +0700

    tambahkan logika switch berdasarkan pilihan user

--- Queues.cpp
+++ Queues.cpp
@@ -132,3 +132,25 @@ int main ()
             cout << "Enter your choice (1-4): " << endl;
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
