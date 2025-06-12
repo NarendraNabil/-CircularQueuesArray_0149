eae241ac9f17f13db22b720c5aa02b5414888c11
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-12 16:49:53 +0700

    jika front_position > rear_position, iterasi dari front hingga akhir array

--- Queues.cpp
+++ Queues.cpp
@@ -94,3 +94,23 @@ public:
         }
         cout << endl;
     }
+    else
+    {
+        // jika front_position > rear_position, iterasi dari front hingga akhir array
+        while (FRONT_position <= max - 1)
+        {
+            cout << queue_array[FRONT_position] << "  ";
+            FRONT_position++;
+        }
+
+        FRONT_position = 0;
+        
+        while ( (FRONT_position <= REAR_position))
+        {
+            cout << queue_array[FRONT_position] << "  ";
+            FRONT_position++;
+        }
+        cout << endl;
+    }  
+    }
+};
